7a0abf9efd7e50243d34d6c53df15d8af4d4f5da
tmlsn <theo.malless@gmail.com>
2021-11-18 17:05:18 +0100

    done

--- src/clue.js
+++ src/clue.js
@@ -2,29 +2,392 @@
 
 // Suspects Array
 
-const suspectsArray = [];
+/* let mrGreen = {
+  firstName: 'Jacob',
+  lastName: 'Green',
+  occupation: 'Entrepreneur',
+  age: 45,
+  description: 'He has a lot of connections',
+  image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+  color: 'green',
+}
+
+let drOrchid = {
+  firstName: 'Doctor',
+  lastName: 'Orchid',
+  occupation: 'Scientist',
+  age: 26,
+  description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+  image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  color: 'white',
+}
+
+let profPlum = {
+  firstName: 'Victor',
+  lastName: 'Plum',
+  occupation: 'Designer',
+  age: 22,
+  description: 'Billionaire video game designer',
+  image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+  color: 'purple',
+}
+
+let missScarlet = {
+  firstName: 'Kasandra',
+  lastName: 'Scarlet',
+  occupation: 'Actor',
+  age: 31,
+  description: 'She is an A-list movie star with a dark past',
+  image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  color: 'red',
+}
+
+let mrsPeacock = {
+  firstName: 'Eleanor',
+  lastName: 'Peacock',
+  occupation: 'Socialité',
+  age: 36,
+  description: 'She is from a wealthy family and uses her status and money to earn popularity',
+  image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+  color: 'blue',
+}
+
+let mrMustard ={
+  firstName: 'Jack',
+  lastName: 'Mustard',
+  occupation: 'Retired Football player',
+  age: 62,
+  description: 'He is a former football player who tries to get by on his former glory',
+  image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+  color: 'yellow',
+}
+
+const suspectsArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
+ */
+
+const suspectsArray = [
+  {
+    firstName: 'Jacob',
+    lastName: 'Green',
+    occupation: 'Entrepreneur',
+    age: 45,
+    description: 'He has a lot of connections',
+    image: 'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+    color: 'green',
+  },
+
+  {
+    firstName: 'Doctor',
+    lastName: 'Orchid',
+    occupation: 'Scientist',
+    age: 26,
+    description: 'PhD in plant toxicology. Adopted daughter of Mr. Boddy',
+    image: 'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'white',
+  },
+
+  {
+    firstName: 'Victor',
+    lastName: 'Plum',
+    occupation: 'Designer',
+    age: 22,
+    description: 'Billionaire video game designer',
+    image: 'https://66.media.tumblr.com/ee7155882178f73b3781603f0908617c/tumblr_phhxc7EhPJ1w5fh03_540.jpg',
+    color: 'purple',
+  },
+
+  {
+    firstName: 'Kasandra',
+    lastName: 'Scarlet',
+    occupation: 'Actor',
+    age: 31,
+    description: 'She is an A-list movie star with a dark past',
+    image: 'https://www.radiotimes.com/uploads/images/Original/111967.jpg',
+    color: 'red',
+  },
+
+  {
+    firstName: 'Eleanor',
+    lastName: 'Peacock',
+    occupation: 'Socialité',
+    age: 36,
+    description: 'She is from a wealthy family and uses her status and money to earn popularity',
+    image: 'https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg',
+    color: 'blue',
+  },
+
+  {
+    firstName: 'Jack',
+    lastName: 'Mustard',
+    occupation: 'Retired Football player',
+    age: 62,
+    description: 'He is a former football player who tries to get by on his former glory',
+    image: 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/07/04/08/unspecified-3.jpg',
+    color: 'yellow',
+  },
+];
+
 
 // Rooms Array
 
-const roomsArray = [];
+/* let room1 = {
+  name : 'Dining Room',
+}
+
+let room2 = {
+  name : 'Conservatory',
+}
+
+let room3 = {
+  name : 'Kitchen',
+}
+
+let room4 = {
+  name : 'Study',
+}
+
+let room5 = {
+  name : 'Library',
+}
+
+let room6 = {
+  name : 'Billiard Room',
+}
+
+let room7 = {
+  name : 'Lounge',
+}
+
+let room8 = {
+  name : 'Ballroom',
+}
+
+let room9 = {
+  name : 'Hall',
+}
+
+let room10 = {
+  name : 'Spa',
+}
+
+let room11 = {
+  name : 'Living Room',
+}
+
+let room12 = {
+  name : 'Observatory',
+}
+
+let room13 = {
+  name : 'Theater',
+}
+
+let room14 = {
+  name : 'Guest House',
+}
+
+let room15 = {
+  name : 'Patio',
+}
+
+const roomsArray = [room1, room2, room3, room4, room5, room6, room7, room8, room9, room10, room11, room12, room13, room14, room15]; */
+
+const roomsArray = [
+  {
+    name : 'Dining Room',
+  },
+
+  {
+    name : 'Conservatory',
+  },
+  
+  {
+    name : 'Kitchen',
+  },
+
+  {
+    name : 'Study',
+  },
+
+  {
+    name : 'Library',
+  },
+
+  {
+    name : 'Billiard Room',
+  },
+
+  {
+    name : 'Lounge',
+  },
+
+  {
+    name : 'Ballroom',
+  },
+
+  {
+    name : 'Hall',
+  },
+
+  {
+    name : 'Spa',
+  },
+
+  {
+    name : 'Living Room',
+  },
+
+  {
+    name : 'Observatory',
+  },
+
+  {
+    name : 'Theater',
+  },
+
+  {
+    name : 'Guest House',
+  },
+
+  {
+    name : 'Patio',
+  },
+];
 
 // Weapons Array
 
-const weaponsArray = [];
+/* let weapon1 = {
+name: 'rope',
+weight: 10,
+}
+
+let weapon2 = {
+name: 'knife',
+weight: 8,
+}
+
+let weapon3 = {
+name: 'candlestick',
+weight: 2,
+}
+
+let weapon4 = {
+name: 'dumbbell',
+weight: 30,
+}
+
+let weapon5 = {
+name: 'poison',
+weight: 2,
+}
+
+let weapon6 = {
+name: 'axe',
+weight: 15,
+}
+
+let weapon7 = {
+name: 'bat',
+weight: 13,
+}
+
+let weapon8 = {
+name: 'trophy',
+weight: 25,
+}
+
+let weapon9 = {
+name: 'pistol',
+weight: 20,
+}
+
+const weaponsArray = [weapon1, weapon2, weapon3, weapon4, weapon5, weapon6, weapon7, weapon8, weapon9]; */
+
+const weaponsArray = [
+ 
+  {
+    name: 'rope',
+    weight: 10,
+    },
+
+  {
+    name: 'knife', 
+    weight: 8,
+    },
+
+  {
+    name: 'candlestick',
+    weight: 2,
+    },
+      
+  {
+    name: 'dumbbell',
+    weight: 30,
+    },
+
+  {
+    name: 'poison',
+    weight: 2,
+    },
+
+  {
+    name: 'axe',
+    weight: 15,
+    },
+
+  {
+    name: 'bat',
+    weight: 13,
+    },
+
+  {
+    name: 'trophy',
+    weight: 25,
+    },
+
+  {
+    name: 'pistol',
+    weight: 20,
+    },
+
+];
+
 
 
 // ITERATION 2
 
-function selectRandom() {}
+function selectRandom(arr) {
+  let posOfSelection = Math.floor(Math.random() * arr.length);
+  let cardSelected = arr[posOfSelection];
+  return cardSelected
+}
 
-function pickMystery() {}
+selectRandom(weaponsArray)
 
+function pickMystery() {
+  let card1 = selectRandom(suspectsArray);
+  let card2 = selectRandom(roomsArray);
+  let card3 = selectRandom(weaponsArray);
+
+  let mystery = {
+    suspect : card1,
+    room : card2,
+    weapon : card3
+  }
+
+  return mystery
+}
+
+// console.log(pickMystery())
 
 // ITERATION 3
 
-function revealMystery() {}
+function revealMystery(mystery) {
+ pickMystery()
+  return `${mystery.suspect.firstName} ${mystery.suspect.lastName} killed Mr. Boddy using the ${mystery.weapon.name} in the ${mystery.room.name}!`
+}
 
-
+// console.log(revealMystery(mystery))
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
